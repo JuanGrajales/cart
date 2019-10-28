31995bf92cd3fa115d34ee0b1c10d05deef93093
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-28 12:14:32 -0400

    add window.load

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,17 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
-}
 
-function calcAll() {
-  // Iteration 1.2
+// anything that require the html file to load before it runs will go inside of this window.onload 
+// if you are not sure then just add everything
+window.onload = ()=> {
+  var $cart = document.querySelector('#cart tbody');
+  var $calc = document.getElementById('calc');
+
+  function updateSubtot($product) {
+    // Iteration 1.1
+  }
+
+  function calcAll() {
+    // Iteration 1.2
+  }
+  $calc.onclick = calcAll;
 }
-$calc.onclick = calcAll;
