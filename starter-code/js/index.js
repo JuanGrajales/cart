7f81be002db3099983023000292c11601505c6c0
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-28 14:04:29 -0400

    update updateSubTot function

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,14 +4,27 @@
 // if you are not sure then just add everything
 window.onload = ()=> {
   var $cart = document.querySelector('#cart tbody');
-  var $calc = document.getElementById('calc');
 
-  function updateSubtot($product) {
-    // Iteration 1.1
+  let updateSubtot = document.querySelector("#calc");
+
+  updateSubtot.onclick = function() {
+    // used .innerText to get the value of that html code
+    let unitPrice = Number(document.querySelector(".pu span").innerText);
+    // inputs used .value not .innerHTML or .innerTEXT
+    let quantity = Number(document.querySelector(".qty label input").value);
+    let subTotal = unitPrice*quantity;
+    document.querySelector(".subtot span").innerText = subTotal;
+
+    /* testing values
+    console.log(unitPrice)
+    console.log(quantity) 
+    console.log(subTotal)
+    console.log(document.querySelector(".subtot span").innerHTML)
+    */
+    return subTotal;
   }
 
   function calcAll() {
     // Iteration 1.2
   }
-  $calc.onclick = calcAll;
 }
