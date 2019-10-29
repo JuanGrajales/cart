fdb035f4bf5aab0743d370fbde3d87e73503fe55
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-29 11:14:39 -0400

    complete calculate all sub-total function

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,36 +1,62 @@
-
-
 // anything that require the html file to load before it runs will go inside of this window.onload 
 // if you are not sure then just add everything
 window.onload = ()=> {
-  var $cart = document.querySelector('#cart tbody');
 
-  let updateSubTot = document.querySelector("#calc");
+  // // this variable points to the button
+  // let updateSubTot = document.querySelector("#calc");
 
-  updateSubTot.onclick = function() {
-    // used .innerText to get the value of that html code
-    let unitPrice = Number(document.querySelector(".pu span").innerText);
-    // inputs used .value not .innerHTML or .innerTEXT
-    let quantity = Number(document.querySelector(".qty label input").value);
-    let subTotal = unitPrice*quantity;
-    document.querySelector(".subtot span").innerText = subTotal;
-    calcAll()
-    /* testing values
-    console.log(unitPrice)
-    console.log(quantity) 
-    console.log(subTotal)
-    console.log(document.querySelector(".subtot span").innerHTML)
-    */
-    return subTotal;
+  // // once the user clicks on the button this will update the sub-total for one item
+  // updateSubTot.onclick = function () {
+  //   // get the value of the unit price
+  //   // use .innerText to get the value of that html code, always return a string
+  //   let unitPrice = Number(document.querySelector(".pu span").innerText);
+
+  //   // get the quantity after user input 
+  //   // inputs used .value not .innerHTML or .innerTEXT
+  //   let quantity = Number(document.querySelector(".qty label input").value);
+
+  //   // calculate the subtotal
+  //   let subTotal = unitPrice*quantity;
+
+  //   // set the value of the sub-total
+  //   document.querySelector(".subtot span").innerText = subTotal;
+
+  //   // testing values
+  //   // console.log(unitPrice)
+  //   // console.log(quantity) 
+  //   // console.log(subTotal)
+  //   // console.log(document.querySelector(".subtot span").innerHTML)
+
+  //   return subTotal;
+  // }
+
+
+
+
+
+
+
+
+
+  let calcAll = document.querySelector("#calc");
+  console.log(calcAll);
+
+  let rows = document.querySelectorAll(".product");
+  console.log(rows);
+
+  calcAll.onclick = function() {
+    // get all of the rows with the class product
+    let rows = document.querySelectorAll(".product");
+    console.log(rows);
+
+    for(let i = 0; i < rows.length; i++) {
+      console.log(rows[i]);
+
+      let unitPrice = Number(rows[i].querySelector('.pu > span').innerText);
+      let quantity = Number(rows[i].querySelector('.qty > label > input').value);
+      let subTotal = rows[i].querySelector('.subtot > span');
+      
+      subTotal.innerText = quantity * unitPrice;
+    }
   }
-
-
-
-
-}
-
-
-
-function calcAll() {
-  // Iteration 1.2
 }
